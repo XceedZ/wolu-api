446d509e4f7164a336f2d9c17dea88d390c20c89
Aditiya WP <aditrizky052@gmail.com>
2024-05-17 10:19:55 +0700

    n=menambah testing di user.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
+     * omaga
      *
      * @var array<int, string>
      */
@@ -20,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'testing'
     ];
 
     /**
